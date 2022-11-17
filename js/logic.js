64a9652e00de867a46db485d20f719881dd698b7
unknown <dbp1795@aim.com>
2022-11-17 13:22:43 -0500

    set variables in js, linked scripts

--- js/logic.js
+++ js/logic.js
@@ -3,5 +3,10 @@ var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 //DOM element variables
-var timerEl = document.getElementById('timer');
+var timerEl = document.getElementById('time');
 var btnstart = document.getElementById('start');
+var btnSubmit = document.getElementById('submit');
+var choicesEl = document.getElementById('choices');
+var questionEl = document.getElementById('questions');
+var feedbackEl = document.getElementById('feedback');
+var nameEl = document.getElementById('playerName');
