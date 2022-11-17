bbcf57e6d43d76e2cf6e76d09eae56ef7582b18c
unknown <dbp1795@aim.com>
2022-11-17 13:00:38 -0500

    added div classes to html for linking js and css

---
+++ js/logic.js
@@ -0,0 +1,7 @@
+// time and question variables
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+//DOM element variables
+var timerEl = document.getElementById('timer');
+var btnstart = document.getElementById('start');
