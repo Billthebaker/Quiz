154b70c003e3b8c603b728d70f7f1e08c9d5c94a
unknown <dbp1795@aim.com>
2022-11-17 22:04:16 -0500

    having trouble with the timer, cannot get high scores to list, fixed issue of quiz not working

--- js/questions.js
+++ js/questions.js
@@ -1,34 +1,34 @@
 var questions = [
     {
-      title: 'Commonly used data types DO NOT include:',
-      choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: 'alerts',
+      title: 'What is The Capital of Djibouti?:',
+      choices: ['Jakarta', 'Riyadh', 'Djibouti', 'Moscow'],
+      answer: 'Djibouti',
     },
     {
-      title: 'The condition in an if / else statement is enclosed within ____.',
-      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-      answer: 'parentheses',
+      title: 'When a Lion Takes Over the Pride, They Often:',
+      choices: ['Mark their Territory', 'Slaughter all the Cubs', 'Have an Election', 'Chase out all the Males'],
+      answer: 'Slaughter all the Cubs',
     },
     {
-      title: 'Arrays in JavaScript can be used to store ____.',
+      title: 'What was the Date of the Philadelphia Experiment?.',
       choices: [
-        'numbers and strings',
-        'other arrays',
-        'booleans',
-        'all of the above',
+        'April 3, 1961',
+        'May 30, 1908',
+        'June 6, 1924',
+        'October 28, 1943',
       ],
-      answer: 'all of the above',
+      answer: 'October 28, 1943',
     },
     {
       title:
-        'String values must be enclosed within ____ when being assigned to variables.',
-      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-      answer: 'quotes',
+        'The Movie: Rocky, was based on the life of which famous boxer after he fought muhammad ali?',
+      choices: ['Chuck Wepner', 'James"Lights-out"Tony', 'Rocky Marciano', 'Butterbean'],
+      answer: 'Chuck Wepner',
     },
     {
       title:
-        'A very useful tool used during development and debugging for printing content to the debugger is:',
-      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+        'A wolf caught in the trap will ____ to survive',
+      choices: ['scream for help', 'chew off its own leg', 'play poker', 'dig a shelter for safety'],
       answer: 'console.log',
     },
   ];
